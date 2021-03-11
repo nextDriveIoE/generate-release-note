5abf4715e03ccd452a8220d68f4b252d31cbae63
amberyoyo <amber.lin@nextdrive.io>
2021-03-11 19:59:32 +0800

    action for generate release note

---
+++ index.js
@@ -0,0 +1,19 @@
+const generator = require('@nextdriveioe/release-note-generator');
+const core = require('@actions/core');
+
+const token = core.getInput('github_token');
+const username = core.getInput('jira_user');
+const password = core.getInput('jira_token');
+const start = core.getInput('base_version');
+const end = core.getInput('current_version');
+const config = core.getInput('config_name');
+
+const generateCommand = new generator.GenerateCommand()
+generateCommand.handler({
+    token,
+    username,
+    password,
+    start,
+    end,
+    config
+})
